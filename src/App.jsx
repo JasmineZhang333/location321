b43bf8d492acf498256948b6165b21d0106b49f9
zhangjiashuai <zhangjiashuai@gmail.com>
2025-03-23 22:01:33 +0800

    icon update

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,8 @@ function App() {
   }, [])
 
   const starIcon = new Icon({
-    iconUrl: '/marker-icon-2x-gold.png',
-    shadowUrl: '/marker-shadow.png',
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
