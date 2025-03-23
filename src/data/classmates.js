a8305cd4a18d56e331650bdc3a74e091caeff306
zhangjiashuai <zhangjs232@chinaunicom.cn>
2025-03-23 17:54:26 +0800

    初始化项目：同学分布地图

---
+++ src/data/classmates.js
@@ -0,0 +1,520 @@
+// 同学位置数据
+export const classmatesData = [
+  {
+    id: 1,
+    name: '曹雅云',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 2,
+    name: '程鹏',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 3,
+    name: '邓美宏',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 4,
+    name: '丁宜涵',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 5,
+    name: '董彦杰',
+    location: {
+      lat: 43.6532,
+      lng: -79.3832
+    },
+    city: '多伦多',
+    country: '加拿大'
+  },
+  {
+    id: 6,
+    name: '董雨莹',
+    location: {
+      lat: 23.1291,
+      lng: 113.2644
+    },
+    city: '广州',
+    country: '中国'
+  },
+  {
+    id: 7,
+    name: '高世超',
+    location: {
+      lat: 22.5431,
+      lng: 114.0579
+    },
+    city: '深圳',
+    country: '中国'
+  },
+  {
+    id: 8,
+    name: '葛晅',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 9,
+    name: '郭城',
+    location: {
+      lat: 22.5431,
+      lng: 114.0579
+    },
+    city: '深圳',
+    country: '中国'
+  },
+  {
+    id: 10,
+    name: '贾畔',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 11,
+    name: '靳秀梅',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 12,
+    name: '李召麒',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 13,
+    name: '梁晟',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 14,
+    name: '梁政渊',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 15,
+    name: '刘梦琪',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 16,
+    name: '刘芝兰',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 17,
+    name: '罗祎宁',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 18,
+    name: '牛锎',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 19,
+    name: '牛悦',
+    location: {
+      lat: 31.2304,
+      lng: 121.4737
+    },
+    city: '上海',
+    country: '中国'
+  },
+  {
+    id: 20,
+    name: '渠慧帆',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 21,
+    name: '石英正',
+    location: {
+      lat: 31.2304,
+      lng: 121.4737
+    },
+    city: '上海',
+    country: '中国'
+  },
+  {
+    id: 22,
+    name: '宋歌',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 23,
+    name: '王博玄',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 24,
+    name: '王嘉沐',
+    location: {
+      lat: 31.2304,
+      lng: 121.4737
+    },
+    city: '上海',
+    country: '中国'
+  },
+  {
+    id: 25,
+    name: '王乐',
+    location: {
+      lat: 31.2304,
+      lng: 121.4737
+    },
+    city: '上海',
+    country: '中国'
+  },
+  {
+    id: 26,
+    name: '王立宸',
+    location: {
+      lat: 42.3601,
+      lng: -71.0589
+    },
+    city: '波士顿',
+    country: '美国'
+  },
+  {
+    id: 27,
+    name: '王敏',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 28,
+    name: '王添',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 29,
+    name: '王雅各',
+    location: {
+      lat: 38.8977,
+      lng: -77.0365
+    },
+    city: '华盛顿',
+    country: '美国'
+  },
+  {
+    id: 30,
+    name: '文治宇',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 31,
+    name: '吴松泽',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 32,
+    name: '薛毅',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 33,
+    name: '闫瑞娟',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 34,
+    name: '闫煜东',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 35,
+    name: '张大千',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 36,
+    name: '张嘉帅',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 37,
+    name: '张乐',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 38,
+    name: '张玲辉',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 39,
+    name: '张婷',
+    location: {
+      lat: 31.2304,
+      lng: 121.4737
+    },
+    city: '上海',
+    country: '中国'
+  },
+  {
+    id: 40,
+    name: '张霄',
+    location: {
+      lat: 30.5728,
+      lng: 104.0668
+    },
+    city: '成都',
+    country: '中国'
+  },
+  {
+    id: 41,
+    name: '张心怡',
+    location: {
+      lat: 37.8706,
+      lng: 112.5489
+    },
+    city: '太原',
+    country: '中国'
+  },
+  {
+    id: 42,
+    name: '张旭彬',
+    location: {
+      lat: 30.5728,
+      lng: 104.0668
+    },
+    city: '成都',
+    country: '中国'
+  },
+  {
+    id: 43,
+    name: '张雅璠',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 44,
+    name: '赵一璇',
+    location: {
+      lat: 31.2304,
+      lng: 121.4737
+    },
+    city: '上海',
+    country: '中国'
+  },
+  {
+    id: 45,
+    name: '赵一峥',
+    location: {
+      lat: 36.7783,
+      lng: -119.4179
+    },
+    city: '加州',
+    country: '美国'
+  },
+  {
+    id: 46,
+    name: '赵自翔',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 47,
+    name: '郑成之',
+    location: {
+      lat: 31.2304,
+      lng: 121.4737
+    },
+    city: '上海',
+    country: '中国'
+  },
+  {
+    id: 48,
+    name: '郑鑫',
+    location: {
+      lat: 39.9042,
+      lng: 116.4074
+    },
+    city: '北京',
+    country: '中国'
+  },
+  {
+    id: 49,
+    name: '周一琦',
+    location: {
+      lat: 22.5431,
+      lng: 114.0579
+    },
+    city: '深圳',
+    country: '中国'
+  }
+];
+
+// 获取统计数据
+export const getStatistics = () => {
+  const countryStats = {};
+  const cityStats = {};
+
+  classmatesData.forEach(classmate => {
+    // 统计国家
+    countryStats[classmate.country] = (countryStats[classmate.country] || 0) + 1;
+    // 统计城市
+    cityStats[classmate.city] = (cityStats[classmate.city] || 0) + 1;
+  });
+
+  // 将统计结果转换为数组并排序
+  const sortedCountryStats = Object.fromEntries(
+    Object.entries(countryStats).sort(([,a], [,b]) => b - a)
+  );
+
+  const sortedCityStats = Object.fromEntries(
+    Object.entries(cityStats).sort(([,a], [,b]) => b - a)
+  );
+
+  return {
+    countryStats: sortedCountryStats,
+    cityStats: sortedCityStats
+  };
+};
